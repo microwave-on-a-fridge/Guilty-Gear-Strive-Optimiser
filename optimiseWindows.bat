5c35a2142f426f3507b51f4fe6ef0b7dba6cacfc
microwave on a fridge <108324217+microwave-on-a-fridge@users.noreply.github.com>
2022-09-29 19:39:19 +1300

    michaelsoft binbows

--- optimiseWindows.bat
+++ optimiseWindows.bat
@@ -1,30 +1,63 @@
 @echo off
 :start
-set unverum=
-set /p unverum=Do you want to use the Unverum mod manager for the pak mods? (Y/N):
-if not '%unverum%'=='' set unverum=%unverum:~0,1%
-if '%unverum%'=='Y' goto yes
-if '%unverum%'=='y' goto yes
-if '%unverum%'=='N' goto no
-if '%unverum%'=='n' goto no
-echo "%unverum%" is not a valid response.
+set dircheck=
+set /p dircheck=Welcome to the Guilty Gear -Strive- Optimiser! Is GGST installed in the default Steam directory (C:\Program Files (x86)\Steam\)? (Tip: If you want to use the .pak mods with the Unverum mod manager, choose "Y")(Y/N):
+if not '%dircheck%'=='' set directory=%dircheck:~0,1%
+if '%dircheck%'=='Y' goto yes
+if '%dircheck%'=='y' goto yes
+if '%dircheck%'=='yes' goto yes
+if '%dircheck%'=='Yes' goto yes
+if '%dircheck%'=='YES' goto yes
+if '%dircheck%'=='N' goto no
+if '%dircheck%'=='n' goto no
+if '%dircheck%'=='no' goto no
+if '%dircheck%'=='No' goto no
+if '%dircheck%'=='NO' goto no
+echo "%dircheck%" is not a valid response. Please try again.
 echo.
 goto start
 
 :no
+set dirr=
+set /p dirr=Please input the full file path to your Steam library (e.g. steamapps) folder where GGST is installed:
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
 copy ".\source\good.ini" "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\"
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\good.ini" "Scalability.ini"
-xcopy /s ".\source\~mods\" "%PROGRAMFILES(X86)%\Steam\steamapps\common\GUILTY GEAR STRIVE\"
+xcopy /s ".\source\~mods\" "%dirr%\common\GUILTY GEAR STRIVE\"
 echo Installation complete.
-@pause
-exit
 
 :yes
+set unverum=
+set /p unverum=Would you like to use the Unverum mod manager for the pak mods? (Y/N):
+if not '%unverum%'=='' set unverum=%unverum:0,1%
+if '%unverum%'=='Y' goto uyes
+if '%directory%'=='y' goto uyes
+if '%directory%'=='yes' goto uyes
+if '%directory%'=='Yes' goto uyes
+if '%directory%'=='YES' goto uyes
+if '%directory%'=='N' goto uno
+if '%directory%'=='n' goto uno
+if '%directory%'=='no' goto uno
+if '%directory%'=='No' goto uno
+if '%directory%'=='NO' goto uno
+echo "%directory%" is not a valid response. Please try again.
+echo.
+goto yes
+
+:uyes
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
 copy ".\source\good.ini" "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\"
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\good.ini" "Scalability.ini"
 xcopy /s /q ".\source\~mods\" ".\Pak Files\"
 echo Installation complete. Pak files have been copied to this directory for use with Unverum.
+@pause
+exit
+
+:uno
+rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
+copy ".\source\good.ini" "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\"
+rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\good.ini" "Scalability.ini"
+xcopy /s ".\source\~mods\" "%PROGRAMFILES(X86)%\Steam\steamapps\common\GUILTY GEAR STRIVE\"
+echo Installation complete.
 @pause
 exit
