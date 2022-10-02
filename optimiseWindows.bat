2017a1da834199596bdc662c2cef6925ceb97198
microwave on a fridge <108324217+microwave-on-a-fridge@users.noreply.github.com>
2022-10-02 19:00:50 +1300

    streamline and simplify

--- optimiseWindows.bat
+++ optimiseWindows.bat
@@ -1,34 +1,7 @@
 @echo off
 :start
-set dircheck=
-set /p dircheck=Welcome to the Guilty Gear -Strive- Optimiser! Is GGST installed in the default Steam directory (C:\Program Files (x86)\Steam\)? (Tip: If you want to use the .pak mods with the Unverum mod manager, choose "Y")(Y/N):
-if not '%dircheck%'=='' set directory=%dircheck:~0,1%
-if '%dircheck%'=='Y' goto yes
-if '%dircheck%'=='y' goto yes
-if '%dircheck%'=='yes' goto yes
-if '%dircheck%'=='Yes' goto yes
-if '%dircheck%'=='YES' goto yes
-if '%dircheck%'=='N' goto no
-if '%dircheck%'=='n' goto no
-if '%dircheck%'=='no' goto no
-if '%dircheck%'=='No' goto no
-if '%dircheck%'=='NO' goto no
-echo "%dircheck%" is not a valid response. Please try again.
-echo.
-goto start
-
-:no
-set dirr=
-set /p dirr=Please input the full file path to your Steam library (e.g. steamapps) folder where GGST is installed:
-rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
-copy ".\source\good.ini" "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\"
-rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\good.ini" "Scalability.ini"
-xcopy /s ".\source\~mods\" "%dirr%\common\GUILTY GEAR STRIVE\"
-echo Installation complete.
-
-:yes
 set unverum=
-set /p unverum=Would you like to use the Unverum mod manager for the pak mods? (Y/N):
+set /p unverum=Welcome to the Guilty Gear -Strive- Optimiser! Would you like to use the Unverum mod manager for the pak mods? (Y/N):
 if not '%unverum%'=='' set unverum=%unverum:0,1%
 if '%unverum%'=='Y' goto uyes
 if '%directory%'=='y' goto uyes
@@ -42,7 +15,7 @@ if '%directory%'=='No' goto uno
 if '%directory%'=='NO' goto uno
 echo "%directory%" is not a valid response. Please try again.
 echo.
-goto yes
+goto start
 
 :uyes
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
@@ -54,6 +27,24 @@ echo Installation complete. Pak files have been copied to this directory for use
 exit
 
 :uno
+set dircheck=
+set /p dircheck=Is GGST installed in the default Steam directory (C:\Program Files (x86)\Steam\)? (Y/N):
+if not '%dircheck%'=='' set directory=%dircheck:~0,1%
+if '%dircheck%'=='Y' goto yes
+if '%dircheck%'=='y' goto yes
+if '%dircheck%'=='yes' goto yes
+if '%dircheck%'=='Yes' goto yes
+if '%dircheck%'=='YES' goto yes
+if '%dircheck%'=='N' goto no
+if '%dircheck%'=='n' goto no
+if '%dircheck%'=='no' goto no
+if '%dircheck%'=='No' goto no
+if '%dircheck%'=='NO' goto no
+echo "%dircheck%" is not a valid response. Please try again.
+echo.
+goto uno
+
+:yes
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
 copy ".\source\good.ini" "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\"
 rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\good.ini" "Scalability.ini"
@@ -61,3 +52,12 @@ xcopy /s ".\source\~mods\" "%PROGRAMFILES(X86)%\Steam\steamapps\common\GUILTY GE
 echo Installation complete.
 @pause
 exit
+
+:no
+set dirr=
+set /p dirr=Please input the full file path to your Steam library (e.g. steamapps) folder where GGST is installed:
+rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\Scalability.ini" "Scalability.ini.bak"
+copy ".\source\good.ini" "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\"
+rename "%LOCALAPPDATA%\GGST\Saved\Config\WindowsNoEditor\good.ini" "Scalability.ini"
+xcopy /s ".\source\~mods\" "%dirr%\common\GUILTY GEAR STRIVE\"
+echo Installation complete.
